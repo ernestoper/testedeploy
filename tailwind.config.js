d2839b6d6f2f42982cc8ccf2f3b2eb9cf926074a
ernestoper <alejojus@gmail.com>
2023-01-21 21:22:24 -0300

    m

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/containers/pages/Home.js}",
+    "./src/containers/pages/Home.jsx",
   ],
   theme: {
     extend: {},
